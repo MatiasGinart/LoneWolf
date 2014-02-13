87a37ac9aa33261e74f0a2b4d1bbd8c5206a34bf
Matías Ginart <mginart@mac-mginart.local>
2014-02-12 23:50:56 -0300

    Initial commit

---
+++ LoneWolf/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LoneWolf
+//
+//  Created by Matías Ginart on 12/02/14.
+//  Copyright (c) 2014 Matías Ginart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LoneWolf/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LoneWolf
+//
+//  Created by Matías Ginart on 12/02/14.
+//  Copyright (c) 2014 Matías Ginart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
